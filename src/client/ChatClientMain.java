b2090f3a8acb2b266eff327e0aa936f2c416033a
chooseanother <kim.tranberg@gmail.com>
2021-03-22 19:50:24 +0100

    Initial setup of files needed

---
+++ src/client/ChatClientMain.java
@@ -0,0 +1,10 @@
+package client;
+
+import javafx.application.Application;
+
+public class ChatClientMain {
+    public static void main(String[] args)
+        {
+            Application.launch(ClientApplication.class);
+        }
+}
---
+++ src/client/viewmodel/LoginViewModel.java
@@ -0,0 +1,15 @@
+package client.viewmodel;
+
+import client.model.Model;
+
+public class LoginViewModel {
+    private Model model;
+
+    public LoginViewModel(Model model){
+        this.model = model;
+    }
+
+    public void clear(){
+
+    }
+}
