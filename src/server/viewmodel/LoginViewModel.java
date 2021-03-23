8bfcb098f64525ff12db48a62b116fbdf2bc5c1b
hanchry <jurco10510@gmail.com>
2021-03-23 10:19:05 +0100

    made basic LoginViewModel with accept method returning only true becouse of non checking if user exist already

--- src/server/viewmodel/LoginViewModel.java
+++ src/server/viewmodel/LoginViewModel.java
@@ -1,15 +1,35 @@
 package server.viewmodel;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import server.model.Model;
 
 public class LoginViewModel {
     private Model model;
+    private StringProperty username, password;
 
     public LoginViewModel(Model model){
         this.model = model;
+        username = new SimpleStringProperty();
+        password = new SimpleStringProperty();
     }
 
     public void clear(){
+        username.setValue(null);
+        password.setValue(null);
+    }
 
+    public String getPassword()
+    {
+        return password.get();
+    }
+
+    public String getUsername()
+    {
+        return username.get();
+    }
+
+    public boolean accept(){
+        return true;
     }
 }
