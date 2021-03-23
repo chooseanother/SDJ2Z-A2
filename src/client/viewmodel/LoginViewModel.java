ceeb1865bd4667ea0674be3f0ef05b6e573e9f19
hanchry <jurco10510@gmail.com>
2021-03-23 10:28:03 +0100

    did the same as in LoginViewModel on server but this time in client

--- src/client/viewmodel/LoginViewModel.java
+++ src/client/viewmodel/LoginViewModel.java
@@ -1,15 +1,36 @@
 package client.viewmodel;
 
 import client.model.Model;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 
 public class LoginViewModel {
     private Model model;
+    private StringProperty username, password;
 
     public LoginViewModel(Model model){
         this.model = model;
+        username = new SimpleStringProperty();
+        password = new SimpleStringProperty();
     }
 
     public void clear(){
+        username.setValue(null);
+        password.setValue(null);
+    }
 
+    public String getPassword()
+    {
+        return password.get();
+    }
+
+    public String getUsername()
+    {
+        return username.get();
+    }
+
+    public boolean accept(){
+        return true;
     }
 }
