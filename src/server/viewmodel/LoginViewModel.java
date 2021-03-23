2a430010b59e5255c73a55055a78a7c5d39d8ac9
hanchry <jurco10510@gmail.com>
2021-03-23 16:26:29 +0100

    changes in LoginViewController and LoginViewModel with buttons and login in

--- src/server/viewmodel/LoginViewModel.java
+++ src/server/viewmodel/LoginViewModel.java
@@ -1,15 +1,59 @@
 package server.viewmodel;
 
+import com.sun.javafx.iio.common.PushbroomScaler;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import server.model.Model;
+
+import javax.swing.*;
 
 public class LoginViewModel {
     private Model model;
+    private StringProperty username, password, error;
 
     public LoginViewModel(Model model){
         this.model = model;
+        username = new SimpleStringProperty();
+        password = new SimpleStringProperty();
+        error = new SimpleStringProperty();
     }
 
     public void clear(){
+        username.setValue(null);
+        password.setValue(null);
+        error.setValue(null);
+    }
 
+    /*public boolean logIn(){
+        if(model.userExist(username.get(), password.get())){
+            return true;
+        }
+        return false;
+    }*/
+
+    public StringProperty getUsername()
+    {
+        return username;
+    }
+
+    public StringProperty getPassword()
+    {
+        return password;
+    }
+
+    public StringProperty getError()
+    {
+        /*if (!logIn()){
+            return error;
+        }
+        else if (!model.userNameExist(username.get())){
+            error.setValue("wrong password");
+            return error;
+        }
+        else {
+            error.setValue("user doesn't exist");
+            return error;
+        }*/
+        return error;
     }
 }
