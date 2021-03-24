7a534ad4e86975f3e5b7557af00d09c427f6dfdf
david <michalcodavid@gmail.com>
2021-03-24 13:20:42 +0100

    Showing messages now works in chatview working on making columns bigger because text doesnt display properly

--- src/server/view/ChatViewController.java
+++ src/server/view/ChatViewController.java
@@ -1,40 +1,41 @@
 package server.view;
 
+import com.sun.source.tree.Tree;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.util.Callback;
 import server.model.Message;
 import server.viewmodel.ChatViewModel;
 import server.viewmodel.SimpleMessagesViewModel;
 
+import javax.swing.*;
 import javax.swing.text.AbstractDocument;
 
-public class ChatViewController extends ViewController{
+public class ChatViewController extends ViewController {
 
-    public TableColumn<SimpleMessagesViewModel,String> Name;
-    public TableColumn<SimpleMessagesViewModel,String> Message;
+    public TableColumn<SimpleMessagesViewModel, String> Name;
+    public TableColumn<SimpleMessagesViewModel, String> Message;
     public TableView<SimpleMessagesViewModel> chatTextArea;
     public TextField TextToBeSent;
-
-    @FXML private Button Send,Log,Logout;
-
+    @FXML
+    private Button Send, Log, Logout;
     @Override
     protected void init() {
         Name.setCellValueFactory(cellData -> cellData.getValue().getUsr());
         Message.setCellValueFactory(cellData -> cellData.getValue().getMsg());
         chatTextArea.setItems(getViewModelFactory().getChatViewModel().getList());
         TextToBeSent.textProperty().bindBidirectional(getViewModelFactory().getChatViewModel().getMsg());
-        chatTextArea.setColumnResizePolicy(TableView.UNCONSTRAINED_RESIZE_POLICY);
-
-
     }
-
-
 
     @Override public void reset(){
         getViewModelFactory().getChatViewModel().clear();
