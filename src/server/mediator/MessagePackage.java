b2612ec050b5ea28d130de840b46a7966bd8735b
chooseanother <kim.tranberg@gmail.com>
2021-03-25 16:05:24 +0100

    Minor changes

--- src/server/mediator/MessagePackage.java
+++ src/server/mediator/MessagePackage.java
@@ -6,6 +6,8 @@ import server.model.Message;
 public class MessagePackage {
     private String type;
     private Message message;
+    private String error;
+    private String password;
 
     public MessagePackage(Message message,String type){
         this.type = type;
